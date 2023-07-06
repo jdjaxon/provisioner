9fe8c0cc297a9bb24bbe1065aaba495d64860f9d
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:43:39 -0400

    Attempting to functionalize provisioner script.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A simple provisioning script to setup a generic development workstation.
 Users will need to provide specific configuration files for a personalized experience.
 
 ## Supported Distribution
-This provisioner was built on and for Ubuntu 20.04.
+This provisioner was built on and for Ubuntu 20.04. Currently, this will only work on distributions with `apt-get`.
--- provisioner.sh
+++ provisioner.sh
@@ -3,12 +3,16 @@
 # This script is intended to stage the host for running ansible,
 # which will finish the provisioning process.
 
-apt update
-# This is a hacky way of setting the time zone information.
-# 2 for America; 105 for New York
-echo "2\n 105\n" | apt install -y software-properties-common
+main() {
+    apt update
+    # This is a hacky way of setting the time zone information.
+    # 2 for America; 105 is for New York
+    echo "2\n 105\n" | apt install -y software-properties-common
 
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install -y ansible git
-ansible --version
-ansible-pull -U https://github.com/jdjaxon/provisioner
+    apt-add-repository --yes --update ppa:ansible/ansible
+    apt install -y ansible git
+    ansible --version
+    ansible-pull -U https://github.com/jdjaxon/provisioner
+}
+
+main
