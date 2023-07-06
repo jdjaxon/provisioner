2beeecbd7281033602542c0b90db20f6c231ebd4
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:43:39 -0400

    First attempt didn't work.

--- provision.sh
+++ provision.sh
@@ -3,11 +3,9 @@
 # This script is intended to stage the host for running ansible,
 # which will finish the provisioning process.
 
-DEBIAN_FRONTEND=noninteractive
-
 main() {
     apt-add-repository --yes --update ppa:ansible/ansible
-    apt install -y ansible git software-properties-common
+    DEBIAN_FRONTEND=noninteractive apt install -y ansible git software-properties-common
     ansible --version
     ansible-pull -U https://github.com/jdjaxon/provisioner
 }
