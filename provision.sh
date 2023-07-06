e1ea287240808cc23b53186822b7deaea8bfe1b0
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:43:40 -0400

    Fixed provisioner back to what I had.

--- provision.sh
+++ provision.sh
@@ -4,8 +4,10 @@
 # which will finish the provisioning process.
 
 main() {
+    apt update
+    DEBIAN_FRONTEND=noninteractive sudo apt install -y software-properties-common
     apt-add-repository -y --update ppa:ansible/ansible
-    DEBIAN_FRONTEND=noninteractive sudo apt install -y ansible git software-properties-common
+    DEBIAN_FRONTEND=noninteractive sudo apt install -y ansible git
     ansible-pull -U https://github.com/jdjaxon/provisioner
 }
 
