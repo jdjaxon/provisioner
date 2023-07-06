cab5326fb5f92db9ad31cb0a271b49e9f76e0cc8
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:43:39 -0400

    Adding changes to provisioner script.

--- provisioner.sh
+++ provisioner.sh
@@ -3,13 +3,12 @@
 # This script is intended to stage the host for running ansible,
 # which will finish the provisioning process.
 
-easy_install pip
-pip install ansible
-
-# TODO: Verify ansible installation worked
-ansible --version
-
 apt update
-apt install software-properties-common
+# This is a hacky way of setting the time zone information.
+# 2 for America; 105 for New York
+echo "2\n 105\n" | apt install -y software-properties-common
+
 apt-add-repository --yes --update ppa:ansible/ansible
-apt install ansible
+apt install -y ansible git
+ansible --version
+ansible-pull -U https://github.com/jdjaxon/provisioner
